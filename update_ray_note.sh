5ec91dacbb298a8c5a9d24fb334b4fee3c4a2d61
rayval <ray.valdes@gmail.com>
2022-11-20 23:15:49 -0800

    pbook4 sunday 1115pm

--- update_ray_note.sh
+++ update_ray_note.sh
@@ -2,8 +2,7 @@ cat line.txt >> ray_notes.txt
 hostname >> ray_notes.txt
 date >> ray_notes.txt
 pwd >> ray_notes.txt
-
-echo "#### updated ray_notes.txt ####
+echo "\n\nUpdated ray_notes.txt!"
 cat ray_notes.txt
 hostname
 

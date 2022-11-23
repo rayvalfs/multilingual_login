9d2d0e997f41f7629e8a3d1248e504d21835ac86
rayval <ray.valdes@gmail.com>
2022-11-22 17:55:41 -0800

    pbook4 tues 556pm

--- update_activity_log.sh
+++ update_activity_log.sh
@@ -2,8 +2,9 @@ cat line.txt >> activity_log.txt
 hostname >> activity./_log.txt
 date >> activity_log.txt
 pwd >> activity_log.txt
-echo "\n\nUpdated activity_log.txt!
 cat activity_log.txt
+echo "\n\nUpdated activity_log.txt!
+echo
 hostname
 
 
